1ef8c788e5de52c51082a3d2564286aea13c7e88
Tiago Mello <tiagomello.info@icloud.com>
2023-03-18 07:51:50 -0300

    refact uvicorn run

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,5 @@ COPY . .
 EXPOSE 8000
 
 #
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", \
+     "--workers", "4", "--limit-concurrency", "70", "--backlog", "300"]
--- main.py
+++ main.py
@@ -29,5 +29,8 @@ if __name__ == '__main__':
 
     # uvicorn.run(
     #     'main:app', host='0.0.0.0', port=80,
-    #     log_level='info', reload=True
+    #     log_level='info', reload=True,
+    #     workers=4, limit_concurrency=70,
+    #     backlog=300
     # )
+
